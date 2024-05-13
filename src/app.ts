a2dd6eb67794940cda57078f755e5861bce43d0e
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2024-05-13 15:12:04 +0300

    disable cors package

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import express, {
   NextFunction,
 } from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import { celebrate, Joi, errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
@@ -20,12 +19,6 @@ import { urlRegEx } from './utils/constants';
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://mesto.domain.students.nomoredomainswork.ru/',
-};
-
-app.use(cors(corsOptions));
-
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,6 +27,22 @@ app.use(helmet());
 mongoose.connect(`${URI}mestodb`);
 
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://mesto.domain.students.nomoredomainswork.ru');
+  res.setHeader('Access-Control-Allow-Headers', [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Credentials',
+    'Authorization',
+    'Cookie',
+  ]);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
